6f089cf00c6aade0c9f6168581286ec1ce6979d5
hique023 <pedroviski10@hotmail.com>
2021-05-16 16:27:51 -0300

    Projeto final

---
+++ src/pages/Home/index.js
@@ -0,0 +1,30 @@
+// Assets
+import { useEffect } from 'react';
+import logo from '../../assets/logo.png'
+import './styles.css'
+
+export default function Home() {
+
+    function assistant() {
+
+    }
+
+    useEffect(() => {
+        assistant()
+    }, [])
+
+
+    return (
+        <div class="container">
+            <div class="containerHeader">
+                <img src={logo} alt="Logo Doc On" />
+                <h1>Olá, seja bem-vindo ao assistente virtual Doc On</h1>
+            </div>
+            <div class="containerFooter">
+                <h2>Por meio de nosso assistente você poderá nos conhecer mais, clique no ícone no canto inferior
+                direito e
+                aproveite!</h2>
+            </div>
+        </div>
+    )
+}
